b3ffb3fed111486ade18fdf1483ea54a24e5932d
Florian Jenn <jennf@tu-cottbus.de>
2013-11-20 11:12:03 +0100

    verlÃ¤ngert bis November

--- make-plots.R
+++ make-plots.R
@@ -1,5 +1,5 @@
 ### flach-tief
 
-plot(window(h[, c("was216", "was224")], start=ISOdate(2011,07,01), end=ISOdate(2013,5,30)), screens = 1, col = c("red", "blue"), ylim = c(36.8, 37.6), main = "flache/tiefe GWMS (rot, blau): WAS216, WAS224", xlab = "", ylab = "h (m NHN)")
+plot(window(h[, c("was216", "was224")], start=ISOdate(2011,07,01), end=ISOdate(2013,11,15)), screens = 1, col = c("red", "blue"), ylim = c(36.6, 37.6), main = "flache/tiefe GWMS (rot, blau): WAS216, WAS224", xlab = "", ylab = "h (m NHN)")
 
-plot(window(h[, c("was209", "was210")], start=ISOdate(2013,05,01), end=ISOdate(2013,9,30)), screens = 1, col = c("red", "blue"), ylim = c(36.7, 37.9), main = "flache/tiefe GWMS (rot, blau): WAS209, WAS210", xlab = "", ylab = "h (m NHN)")
+plot(window(h[, c("was209", "was210")], start=ISOdate(2013,05,01), end=ISOdate(2013,11,15)), screens = 1, col = c("red", "blue"), ylim = c(36.7, 37.9), main = "flache/tiefe GWMS (rot, blau): WAS209, WAS210", xlab = "", ylab = "h (m NHN)")
